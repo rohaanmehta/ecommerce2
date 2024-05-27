39ab76b0879fb10cdfc99292357a501df3e2b952
Amit Pashte <amitpashte2007@gmail.com>
2024-05-27 18:30:48 +0530

    reviews 2

--- app/Views/Shop/page/product_page.php
+++ app/Views/Shop/page/product_page.php
@@ -561,6 +561,18 @@
                         </div>
                     </div>
                 </div>
+                <div class="card">
+                    <div class="card-head" id="heading3">
+                        <p class="mb-0 accordion-title collapsed" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                            Reviews
+                        </p>
+                    </div>
+                    <div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionExample">
+                        <div class="card-body accordion-desc">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
